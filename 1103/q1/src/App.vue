925baf5e3f7bfa5b0696230fea2258c94210f9d9
yr7256 <yr7256@gmail.com>
2022-11-03 14:38:16 +0900

    button_box

---
+++ 1103/q1/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <h1>버튼 박스 제작</h1>
+    <div class="box">
+      <h2>예약 페이지</h2>
+      <h3>시간 선택</h3>
+      <div>
+        <button class="btn" @click="append(time)" v-for="(time, index) in times" :key="index" :class="{'clicked': selects.includes(time)}">{{ time }}</button>
+      </div>
+      <br>
+      <hr>
+      <h3>선택 시간 :
+        <span v-for="(select,index) in selects" :key="index"> {{ select }}</span>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      times: [
+        "00:00","00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30",
+        "05:00","05:30","06:00","06:30","07:00","07:30","08:00","08:30","09:00","09:30",
+        "10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30",
+        "15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30", "19:00","19:30",
+        "20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30",
+      ],
+      selects: [],
+    }
+  },
+  methods: {
+    append: function (num) {
+      if (this.selects.length < 5 && this.selects.includes(num)===false) {
+        this.selects.push(num)
+      } else if (this.selects.includes(num)===true){
+        const idx = this.selects.indexOf(num)
+        this.selects.splice(idx, 1)
+      } else {
+        alert("5타임까지만 신청할 수 있습니다.")
+      }
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Noto Sans Korean', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #424242;
+  margin-top: 60px;
+}
+.box {
+  box-shadow: 0px 2px 5px 0px #84898c;
+  width: 400px;
+  margin: auto;
+  padding: 10px 0px 10px 0px;
+}
+.btn {
+  background: white;
+  border: 0;
+  outline: 0;
+  color: #84898c;
+}
+.clicked {
+  color: #0F4C81;
+  background: #658dc63d;
+}
+hr {
+  width: 75%;
+}
+</style>
