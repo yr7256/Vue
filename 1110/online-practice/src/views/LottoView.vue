6474114e7ea1f3f74f4d4be49e744e72eedd5a61
yr7256 <yr7256@gmail.com>
2022-11-10 17:25:13 +0900

    online_practice

---
+++ 1110/online-practice/src/views/LottoView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="lotto">
+    <h1>로또</h1>
+    <button @click="toGetNumbers">Pick Lucky Numbers</button><br>
+    <br>
+    {{ nums }}
+  </div>
+</template>
+
+<script>
+import _ from "lodash"
+
+export default {
+  name: "LottoView",
+  data() {
+    return {
+      nums: null
+    }
+  },
+  components: {
+
+  },
+  methods: {
+    toGetNumbers() {
+      this.nums = _.sampleSize(_.range(1,47), 6)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 1110/online-practice/src/views/LunchView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="lunch">
+    <h1>점심메뉴</h1>
+    <button @click="toLunch">Pick Lunch</button>
+    <p>{{ pickedMenu }}</p>
+    <br>
+    <h2>로또를 뽑아보자</h2>
+    <button @click="toLotto">Pick Lotto</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LunchView',
+  data() {
+    return {
+      menus : ['떡볶이', '국밥', '치킨', '피자', '족발', '짜장면', '짬뽕', '김치찌개'],
+      pickedMenu : null,
+    }
+  },
+  components: {
+    
+  },
+  methods: {
+    toLunch() {
+      this.pickedMenu = this.menus[Math.floor(Math.random() * this.menus.length)];
+    },
+    toLotto() {
+      this.$router.push({ name: 'lotto' })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
